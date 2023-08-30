69d06f5473486a860e4bf39e2b5b5993e61ebe50
Sowundarajraja <142381790+Sowundarajraja@users.noreply.github.com>
2023-08-30 15:04:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('FROST & SULLIVAN HOME'); //write a response to the client
+  res.write('HI SOWUNDAR'); //write a response to the client
   res.end(); //end the response
 }).listen(80); //the server object listens on port 80
